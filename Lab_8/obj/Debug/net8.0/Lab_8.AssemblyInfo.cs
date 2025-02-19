40f82933cdc36bbc1c2d89dcaf55e1e8b4111897
Khangdh162 <155737062+Khangdh162@users.noreply.github.com>
2025-02-20 03:50:06 +0700

    Update

--- Lab_8/obj/Debug/net8.0/Lab_8.AssemblyInfo.cs
+++ Lab_8/obj/Debug/net8.0/Lab_8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31074d8fa5384971a320376c83f3fda673ea0e33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
